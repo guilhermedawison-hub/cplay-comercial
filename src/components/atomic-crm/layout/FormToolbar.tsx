f92d1bc1ee8b0f599e7d246dedca21c7ca7259e4
Guilherme Dawison <guilhermedawison@gmail.com>
2026-09-17 17:34:05 -0300

    refactor: allow CPlay toolbar styling

--- src/components/atomic-crm/layout/FormToolbar.tsx
+++ src/components/atomic-crm/layout/FormToolbar.tsx
@@ -1,10 +1,14 @@
 import { CancelButton } from "@/components/admin/cancel-button";
 import { SaveButton } from "@/components/admin/form";
+import { cn } from "@/lib/utils";
 
-export const FormToolbar = () => (
+export const FormToolbar = ({ className }: { className?: string }) => (
   <div
     role="toolbar"
-    className="sticky flex pt-4 pb-4 md:pb-0 bottom-0 bg-linear-to-b from-transparent to-card to-10% flex-row justify-end gap-2"
+    className={cn(
+      "sticky bottom-0 flex flex-row justify-end gap-2 bg-linear-to-b from-transparent to-card to-10% pb-4 pt-4 md:pb-0",
+      className,
+    )}
   >
     <CancelButton />
     <SaveButton />
